dc8cef71925444e9f3e6b9336c108506ee15c63d
Maxim <mail@maxim-meier.de>
2021-06-04 16:03:58 +0200

    added commments

--- fractalTreeScript.js
+++ fractalTreeScript.js
@@ -6,34 +6,32 @@ canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 
 function drawTree(startX, startY, len, angle, branchWidth, color1, color2,) {
-    if (len < getRandomArbitrary(1, 20)) {
+    if (len < getRandomArbitrary(1, 20)) { //change behaviour after a certain length
         ctx.strokeStyle = color2;
         ctx.fillStyle = color2;
         branchWidth *= getRandomArbitrary(1, 4);
-    } else {
+    } else {  //if no specific length if reached
         ctx.strokeStyle = color1;
         ctx.fillStyle = color1;
     }
     ctx.beginPath();
     ctx.save();
-    color1 = color1;
-    color2 = color2;
     ctx.lineWidth = branchWidth;
     ctx.translate(startX, startY);
     ctx.rotate(angle * Math.PI/getRandomArbitrary(60, 350));
     ctx.moveTo(0, 0);
     ctx.lineTo(0, -len);
     ctx.stroke();
-    if (len < 10) {
+    if (len < 10) { //length limiter
         ctx.restore();
         return;
     }
-    drawTree(0, -len, len*0.8, angle+6, branchWidth * 0.7, color1, color2);
-    drawTree(0, -len, len*0.8, angle-6, branchWidth * 0.7, color1, color2);
+    drawTree(0, -len, len*0.8, angle+6, branchWidth * 0.7, color1, color2); // generate two more lines out of one
+    drawTree(0, -len, len*0.8, angle-6, branchWidth * 0.7, color1, color2); //
     ctx.restore();
 }
 
-function getRandomArbitrary(min, max) { //getRandomArbitrary(1, 4)
+function getRandomArbitrary(min, max) { 
     return Math.random() * (max - min) + min;
 }
 
